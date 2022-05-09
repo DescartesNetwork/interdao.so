e54f0a674f7ccfe53d31d24256910f94597e2534
thiennv <thiennguyen@descartes.network>
2022-05-09 18:04:15 +0700

    valid

--- src/app/view/dao/daoInitialization/actions/continuesAction.tsx
+++ src/app/view/dao/daoInitialization/actions/continuesAction.tsx
@@ -6,15 +6,22 @@ const ContinuesAction = ({
   step,
   onHandleStep,
   onConfirm,
+  disabled = false,
+  loading = false,
 }: CreateDaoTitleProps) => {
   if (step === CreateSteps.stepThree)
     return (
-      <Button onClick={onConfirm} type="primary" size="large">
+      <Button onClick={onConfirm} type="primary" size="large" loading={loading}>
         Confirm
       </Button>
     )
   return (
-    <Button onClick={onHandleStep} type="primary" size="large">
+    <Button
+      onClick={onHandleStep}
+      type="primary"
+      size="large"
+      disabled={disabled}
+    >
       Continues
     </Button>
   )
