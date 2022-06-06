3f4f1733fe5a7ce7ba776a61734988fff15cbdfa
The Vinh <vinhnguyen@descartes.network>
2022-06-06 14:56:51 +0700

    update size

--- src/app/view/dao/daoInitialization/components/privacy.tsx
+++ src/app/view/dao/daoInitialization/components/privacy.tsx
@@ -1,6 +1,5 @@
 import { useDispatch, useSelector } from 'react-redux'
 import isEqual from 'react-fast-compare'
-import { useUI } from '@senhub/providers'
 
 import { Button, Col, Row, Typography } from 'antd'
 import { AppDispatch, AppState } from 'app/model'
@@ -10,11 +9,6 @@ const Privacy = () => {
   const initDao = useSelector((state: AppState) => state.dao.initDao)
   const { isPublic } = initDao
   const dispatch = useDispatch<AppDispatch>()
-  const {
-    ui: { infix },
-  } = useUI()
-  const mobileScreen = infix === 'xs'
-  const mobileSpan = mobileScreen ? 8 : undefined
 
   const handleClick = (isPublic: boolean) => {
     return dispatch(setInitDao({ ...initDao, isPublic }))
@@ -24,7 +18,7 @@ const Privacy = () => {
       <Col span={24}>
         <Typography.Text>DAO privacy</Typography.Text>
       </Col>
-      <Col span={mobileSpan}>
+      <Col>
         <Button
           onClick={() => handleClick(false)}
           className={isEqual(isPublic, false) ? '' : 'btn-unselect'}
@@ -33,7 +27,7 @@ const Privacy = () => {
           Member Only
         </Button>
       </Col>
-      <Col span={mobileSpan}>
+      <Col>
         <Button
           onClick={() => handleClick(true)}
           className={isEqual(isPublic, true) ? '' : 'btn-unselect'}
