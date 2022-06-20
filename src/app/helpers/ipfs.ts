733baca9a2a126effec9b28a38a8fb7669ed60ca
minhcong <83624415+dmcong@users.noreply.github.com>
2022-06-20 15:31:38 +0700

    update loading

--- src/app/helpers/ipfs.ts
+++ src/app/helpers/ipfs.ts
@@ -1,5 +1,7 @@
 import { Web3Storage } from 'web3.storage'
 
+import { DataLoader } from 'shared/dataloader'
+
 const KEY =
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweDg4MzdCZUI2ODM5MTcwODZjQUI3OTU0MzI3ZTgwOWU1ZTlCZTc2NTEiLCJpc3MiOiJ3ZWIzLXN0b3JhZ2UiLCJpYXQiOjE2NTU0NTU5NzI5MjAsIm5hbWUiOiJTZW50cmUifQ.Jf7oQOKMrBxp5morvs7DR_As4EU9Y5WybyuvY1teFN8'
 
@@ -20,21 +22,23 @@ class IPFS {
   }
 
   get = async <T>(cid: string): Promise<T> => {
-    const re = await this.provider.get(cid)
-    const file = ((await re?.files()) || [])[0]
-    const reader = new FileReader()
-    return new Promise((resolve, reject) => {
-      try {
-        if (!file) throw new Error('Cannot read empty file')
-        reader.onload = () => {
-          const contents = reader.result?.toString()
-          if (!contents) throw new Error('Cannot read empty file')
-          return resolve(JSON.parse(contents))
+    return DataLoader.load<T>(`ipfs^${cid}`, async () => {
+      const re = await this.provider.get(cid)
+      const file = ((await re?.files()) || [])[0]
+      const reader = new FileReader()
+      return new Promise((resolve, reject) => {
+        try {
+          if (!file) throw new Error('Cannot read empty file')
+          reader.onload = () => {
+            const contents = reader.result?.toString()
+            if (!contents) throw new Error('Cannot read empty file')
+            return resolve(JSON.parse(contents))
+          }
+          reader.readAsText(file)
+        } catch (er: any) {
+          return reject(er.message)
         }
-        reader.readAsText(file)
-      } catch (er: any) {
-        return reject(er.message)
-      }
+      })
     })
   }
 }
--- src/app/view/dao/daoDetails/daoInfo.tsx
+++ src/app/view/dao/daoDetails/daoInfo.tsx
@@ -4,7 +4,7 @@ import { SystemProgram } from '@solana/web3.js'
 import BN from 'bn.js'
 import { useUI, useWallet } from '@senhub/providers'
 
-import { Avatar, Button, Card, Col, Row, Space, Typography } from 'antd'
+import { Avatar, Button, Card, Col, Row, Space, Spin, Typography } from 'antd'
 import IonIcon from '@sentre/antd-ionicon'
 import StatisticCard from 'app/components/statisticCard'
 import RegimeTag from 'app/components/regimeTag'
@@ -45,107 +45,109 @@ const DaoInfo = ({ daoAddress }: DaoDetailsProps) => {
   const mobileScreen = width < 768
 
   return (
-    <Card bordered={false} loading={loading}>
-      <Row gutter={[16, 16]}>
-        <Col span={24}>
-          <Row justify="space-between" wrap={false}>
-            <Col>
-              <Row gutter={[16, 16]} wrap={false}>
-                <Col>
-                  {!metaData?.image ? (
-                    <GradientAvatar
-                      seed={daoAddress}
-                      avatarProps={{ shape: 'square', size: 56 }}
-                    />
-                  ) : (
-                    <Avatar shape="square" size={56} src={metaData?.image} />
-                  )}
-                </Col>
-                <Col flex={mobileScreen ? 'auto' : undefined}>
-                  <Space direction="vertical" size={0}>
-                    <Typography.Title level={4}>
-                      {metaData?.daoName
-                        ? metaData.daoName
-                        : shortenAddress(daoAddress)}
-                    </Typography.Title>
-                    <Space size={0} style={{ marginLeft: -3 }}>
-                      {(metaData?.optionals || []).map(
-                        (url, idx) =>
-                          validURL(url) && (
-                            <Button
-                              size="small"
-                              type="text"
-                              onClick={() => window.open(url, '_blank')}
-                              icon={<IonIcon name={getIcon(url)} />}
-                              key={idx}
-                            />
-                          ),
-                      )}
+    <Spin spinning={loading} tip="Loading...">
+      <Card bordered={false}>
+        <Row gutter={[16, 16]}>
+          <Col span={24}>
+            <Row justify="space-between" wrap={false}>
+              <Col>
+                <Row gutter={[16, 16]} wrap={false}>
+                  <Col>
+                    {!metaData?.image ? (
+                      <GradientAvatar
+                        seed={daoAddress}
+                        avatarProps={{ shape: 'square', size: 56 }}
+                      />
+                    ) : (
+                      <Avatar shape="square" size={56} src={metaData?.image} />
+                    )}
+                  </Col>
+                  <Col flex={mobileScreen ? 'auto' : undefined}>
+                    <Space direction="vertical" size={0}>
+                      <Typography.Title level={4}>
+                        {metaData?.daoName
+                          ? metaData.daoName
+                          : shortenAddress(daoAddress)}
+                      </Typography.Title>
+                      <Space size={0} style={{ marginLeft: -3 }}>
+                        {(metaData?.optionals || []).map(
+                          (url, idx) =>
+                            validURL(url) && (
+                              <Button
+                                size="small"
+                                type="text"
+                                onClick={() => window.open(url, '_blank')}
+                                icon={<IonIcon name={getIcon(url)} />}
+                                key={idx}
+                              />
+                            ),
+                        )}
+                      </Space>
                     </Space>
-                  </Space>
-                </Col>
-              </Row>
-            </Col>
-            <Col>
-              <Button
-                type="text"
-                size="large"
-                onClick={editDAO}
-                disabled={authority && walletAddress !== authority.toBase58()}
-                style={{
-                  marginRight: -10,
-                  marginTop: -20,
-                }}
-                icon={<IonIcon name="open-outline" />}
-              />
-            </Col>
-          </Row>
-        </Col>
-        <Col span={24}>
-          <Row gutter={[36, 16]}>
-            <Col xs={12} sm={6}>
-              <StatisticCard
-                title="Proposals"
-                value={numeric(Number(nonce)).format('0,0')}
-              />
-            </Col>
-            <Col xs={12} sm={6}>
-              <AmountMembers daoAddress={daoAddress} />
-            </Col>
-            <Col xs={12} sm={6}>
-              <StatisticCard
-                title="Vote by"
-                value={
-                  isNft ? (
-                    'NFT'
-                  ) : (
-                    <Space>
-                      <MintAvatar mintAddress={mint.toBase58()} />
-                      <MintSymbol mintAddress={mint.toBase58()} />
-                    </Space>
-                  )
-                }
-              />
-            </Col>
-            <Col xs={12} sm={6}>
-              <StatisticCard
-                title="Regime"
-                value={<RegimeTag regime={regime} />}
-              />
-            </Col>
-          </Row>
-        </Col>
-        <Col span={24} className="scrollbar" style={{ height: 66 }}>
-          <Typography.Paragraph
-            type="secondary"
-            ellipsis={{ rows: 3, expandable: true, symbol: 'View more' }}
-            style={{ margin: 0 }}
-          >
-            {metaData?.description}
-          </Typography.Paragraph>
-        </Col>
-      </Row>
-    </Card>
+                  </Col>
+                </Row>
+              </Col>
+              <Col>
+                <Button
+                  type="text"
+                  size="large"
+                  onClick={editDAO}
+                  disabled={authority && walletAddress !== authority.toBase58()}
+                  style={{
+                    marginRight: -10,
+                    marginTop: -20,
+                  }}
+                  icon={<IonIcon name="open-outline" />}
+                />
+              </Col>
+            </Row>
+          </Col>
+          <Col span={24}>
+            <Row gutter={[36, 16]}>
+              <Col xs={12} sm={6}>
+                <StatisticCard
+                  title="Proposals"
+                  value={numeric(Number(nonce)).format('0,0')}
+                />
+              </Col>
+              <Col xs={12} sm={6}>
+                <AmountMembers daoAddress={daoAddress} />
+              </Col>
+              <Col xs={12} sm={6}>
+                <StatisticCard
+                  title="Vote by"
+                  value={
+                    isNft ? (
+                      'NFT'
+                    ) : (
+                      <Space>
+                        <MintAvatar mintAddress={mint.toBase58()} />
+                        <MintSymbol mintAddress={mint.toBase58()} />
+                      </Space>
+                    )
+                  }
+                />
+              </Col>
+              <Col xs={12} sm={6}>
+                <StatisticCard
+                  title="Regime"
+                  value={<RegimeTag regime={regime} />}
+                />
+              </Col>
+            </Row>
+          </Col>
+          <Col span={24} className="scrollbar" style={{ height: 66 }}>
+            <Typography.Paragraph
+              type="secondary"
+              ellipsis={{ rows: 3, expandable: true, symbol: 'View more' }}
+              style={{ margin: 0 }}
+            >
+              {metaData?.description}
+            </Typography.Paragraph>
+          </Col>
+        </Row>
+      </Card>
+    </Spin>
   )
 }
 
