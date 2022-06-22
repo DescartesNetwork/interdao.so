29d84367a780e6901a7726a0cf93f6e1b08a7f10
minhcong <83624415+dmcong@users.noreply.github.com>
2022-06-22 07:59:43 +0700

    update onConfirm template approve

--- src/app/templates/spl-approve/create.tsx
+++ src/app/templates/spl-approve/create.tsx
@@ -1,39 +1,31 @@
-import { useCallback, useEffect } from 'react'
+import { useCallback } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 
 import { Button, Col, Row, Space } from 'antd'
 import { NumberInput, MintInput, AddressInput } from 'app/templates/components'
 
 import { AppDispatch, AppState } from 'app/model'
-import { setTemplateData } from 'app/model/template.controller'
-import useMetaData from 'app/hooks/useMetaData'
 import { SplApproveIdl, SplApproveIds } from '../spl-approve/configs'
 import { PropsCreateComponent } from '../index'
+import useMetaData from 'app/hooks/useMetaData'
 import { useConfirmIdl } from '../hooks/useConfirmIdl'
+import { setTemplateData } from 'app/model/template.controller'
 
 const Create = ({ daoAddress = '' }: PropsCreateComponent) => {
   const dispatch = useDispatch<AppDispatch>()
   const daoData = useSelector((state: AppState) => state.daos[daoAddress])
   const { metaData: daoMetaData } = useMetaData(daoAddress)
+
   const { confirm, close } = useConfirmIdl()
 
-  const generateData = useCallback(async () => {
-    dispatch(
-      setTemplateData({
-        id: SplApproveIds.authority,
-        value: daoData.master.toBase58(),
-      }),
-    )
-    dispatch(
-      setTemplateData({
-        id: SplApproveIds.code,
-        value: '4',
-      }),
-    )
-  }, [daoData.master, dispatch])
-  useEffect(() => {
-    generateData()
-  }, [generateData])
+  const onConfirm = useCallback(async () => {
+    const defaultData = {
+      [SplApproveIds.code]: '4',
+      [SplApproveIds.authority]: daoData.master.toBase58(),
+    }
+    await dispatch(setTemplateData(defaultData))
+    return confirm(SplApproveIdl)
+  }, [confirm, daoData.master, dispatch])
 
   return (
     <Row gutter={[24, 24]}>
@@ -65,11 +57,7 @@ const Create = ({ daoAddress = '' }: PropsCreateComponent) => {
           <Button type="text" onClick={close}>
             Close
           </Button>
-          <Button
-            type="primary"
-            onClick={() => confirm(SplApproveIdl)}
-            disabled={false}
-          >
+          <Button type="primary" onClick={onConfirm} disabled={false}>
             Continue
           </Button>
         </Space>
