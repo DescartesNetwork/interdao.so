b463135757e733b4510beaf1534324faa7a562fd
thiennv <thiennguyen@descartes.network>
2022-07-14 15:10:14 +0700

    get holders from solscan public api

--- src/hooks/dao/useDaoMembers.ts
+++ src/hooks/dao/useDaoMembers.ts
@@ -1,8 +1,10 @@
 import { useDaoData } from 'hooks/dao'
 import { useCallback, useEffect, useState } from 'react'
 import { useDispatch } from 'react-redux'
+import { net } from '@sentre/senhub'
 
 import { AppDispatch } from 'model'
+import { fetchTokenHolders } from 'helpers/tokenHolders'
 import { getTokenHolders } from 'model/tokenHolder.controller'
 
 export const useDaoMembers = (daoAddress: string): number => {
@@ -10,15 +12,24 @@ export const useDaoMembers = (daoAddress: string): number => {
   const daoData = useDaoData(daoAddress)
   const [members, setMembers] = useState(0)
 
-  const fetchDaoMember = useCallback(async () => {
+  const fetchDaoMemberFromSolScan = useCallback(async () => {
+    const mintAddr = daoData?.mint.toBase58() || ''
+    const members = await fetchTokenHolders(mintAddr)
+
+    setMembers(members)
+  }, [daoData?.mint])
+
+  const fecthDaoMemberFromStore = useCallback(async () => {
     const data = await dispatch(getTokenHolders({ daoAddress })).unwrap()
     const members = data[daoAddress] || 0
+
     setMembers(members)
   }, [daoAddress, dispatch])
 
   useEffect(() => {
-    if (daoData) fetchDaoMember()
-  }, [daoData, fetchDaoMember])
+    if (daoData && net === 'mainnet') fetchDaoMemberFromSolScan()
+    if (daoData && net === 'devnet') fecthDaoMemberFromStore()
+  }, [daoData, fecthDaoMemberFromStore, fetchDaoMemberFromSolScan])
 
   return members
 }
